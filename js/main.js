18e49bb2a6ed1f26695556bee426ac3c1871dcb9
LetMeBeMe <43950139+LetMeBeMe@users.noreply.github.com>
2020-07-27 21:29:59 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* ввел ли пользователь пароль */
 window.onload = function() {
   if(localStorage.getItem('pwd') != 'true') {
-    window.location.assign('index');
+    window.location.assign('https://markjoulith.com/');
   }
 }
 
